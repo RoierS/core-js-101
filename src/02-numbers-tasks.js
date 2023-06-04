281973a4e0e3b408ead3108e4d8a27a779aae0d5
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-06-04 19:13:15 +0200

    feat: done number-task 7

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -125,8 +125,9 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  const arr = value.toString().split('');
+  return arr[arr.length - 1];
 }
 
 
