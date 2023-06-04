dae82cf286dcd8bef23f22eb94e8d0790f1ecbd7
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-06-04 18:14:49 +0200

    feat: done string-task 15

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -288,8 +288,15 @@ function isString(value) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const deck = ['A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣', 'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦', 'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥', 'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠'];
+  let result;
+  for (let i = 0; i < deck.length; i += 1) {
+    if (deck[i] === value) {
+      result = deck.indexOf(deck[i]);
+    }
+  }
+  return result;
 }
 
 
