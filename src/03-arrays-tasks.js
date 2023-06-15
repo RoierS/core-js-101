62186c9fbe3f2d452b0085487602781294b02785
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-06-15 11:59:49 +0200

    feat: done array-tasks findAllOccurrences

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -369,8 +369,9 @@ function getFalsyValuesCount(arr) {
  *    [ null, undefined, null ], null => 2
  *    [ true, 0, 1, 'true' ], true => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  const res = arr.filter((el) => el === item);
+  return res.length;
 }
 
 /**
