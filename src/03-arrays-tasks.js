2c1ec2ceaaeb3433c5c44a5901a62de2fff6c91c
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-06-15 11:42:44 +0200

    feat: done array-tasks getPositivesCount

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -300,8 +300,9 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  const res = arr.filter((el) => el > 0 && typeof el === 'number');
+  return res.length;
 }
 
 /**
