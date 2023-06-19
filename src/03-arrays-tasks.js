e8d40cef07e334c9b3df46f89199f6eb7aa5ec94
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-06-19 11:49:28 +0200

    feat: done array-tasks getElementByIndexes

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -565,8 +565,8 @@ function selectMany(arr, childrenSelector) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  return indexes.reduce((res, index) => res[index], arr);
 }
 
 /**
@@ -587,8 +587,8 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+
 }
 
 module.exports = {
