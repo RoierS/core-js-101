45ecafcf18198d222f8fd61dfe0e3d9e4a64c001
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-06-23 11:29:30 +0200

    feat: done 08-functions-n-closures-tasks getIdGeneratorFunction

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -190,8 +190,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom;
+  return function getId() {
+    const res = id;
+    id += 1;
+    return res;
+  };
 }
 
 
