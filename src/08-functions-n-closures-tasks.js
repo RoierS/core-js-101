2212543962d3d4bbf143616fce2e52f05cf735ee
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-06-23 12:38:34 +0200

    feat: done 08-functions-n-closures-tasks logger

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -165,8 +165,13 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function logHelper(...args) {
+    logFunc(`${func.name}(${JSON.stringify(args).slice(1, -1)}) starts`);
+    const res = func.apply(this, args);
+    logFunc(`${func.name}(${JSON.stringify(args).slice(1, -1)}) ends`);
+    return res;
+  };
 }
 
 
