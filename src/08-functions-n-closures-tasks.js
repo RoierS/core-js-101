c9a4f409b0a140c87863932af4c60067b46af230
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-06-23 11:14:15 +0200

    feat: done 08-functions-n-closures-tasks getPolynom

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -66,8 +66,14 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...coefficients) {
+  return function polynom(x) {
+    let res = 0;
+    for (let i = 0; i < coefficients.length; i += 1) {
+      res += coefficients[i] * (x ** (coefficients.length - 1 - i));
+    }
+    return res;
+  };
 }
 
 
