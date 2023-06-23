bb3cf73378380342c74223df4bfe8fd1340b27f1
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-06-23 11:22:15 +0200

    feat: done 08-functions-n-closures-tasks memoize

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -91,8 +91,17 @@ function getPolynom(...coefficients) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cache = null;
+  let hasRes = false;
+
+  return function memoFunc() {
+    if (!hasRes) {
+      cache = func();
+      hasRes = true;
+    }
+    return cache;
+  };
 }
 
 
