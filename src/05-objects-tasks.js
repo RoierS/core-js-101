3e658505d907632cae6c8015d4705eb51989759d
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-07-03 19:01:11 +0200

    feat: almost all tests clear exepts one for 05-objects-tasks cssBuilder

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -115,174 +115,116 @@ function fromJSON(proto, json) {
  *  For more examples see unit tests.
  */
 
-// const cssSelectorBuilder = {
-//   selectors: [],
-
-//   element(value) {
-//     this.addSelector(value, 1);
-//     return this;
-//   },
-
-//   id(value) {
-//     this.addSelector(`#${value}`, 2);
-//     return this;
-//   },
-
-//   class(value) {
-//     this.addSelector(`.${value}`, 3);
-//     return this;
-//   },
-
-//   attr(value) {
-//     this.addSelector(`[${value}]`, 4);
-//     return this;
-//   },
-
-//   pseudoClass(value) {
-//     this.addSelector(`:${value}`, 5);
-//     return this;
-//   },
-
-//   pseudoElement(value) {
-//     this.addSelector(`::${value}`, 6);
-//     return this;
-//   },
-
-//   combine(selector1, combinator, selector2) {
-//     this.addSelector(`${selector1.stringify()} ${combinator} ${selector2.stringify()}`, 7);
-//     return this;
-//   },
-
-//   stringify() {
-//     const res = this.selectors.join('');
-//     this.selectors = [];
-//     return res;
-//   },
-
-//   addSelector(selector, priority) {
-//     const newBuild = Object.create(cssSelectorBuilder);
-//     newBuild.selectors = [...this.selectors, { selector, priority }];
-//     return newBuild;
-//   },
-// };
-
-class CssSelectorBuilder {
+class Selector {
   constructor() {
-    this.selectors = [];
-    // this.elementCount = 0;
-    // this.idCount = 0;
-    // this.pseudoElementCount = 0;
+    this.elementValue = '';
+    this.idValue = '';
+    this.classValues = [];
+    this.attrValue = '';
+    this.pseudoClassValues = [];
+    this.pseudoElementValue = '';
+    this.order = 0;
   }
 
   element(value) {
+    if (this.elementValue) {
+      throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    }
+    this.elementValue = value;
     this.validateOrder(1);
-    this.elementCount += 1;
-    this.selectors.push(value);
     return this;
   }
 
   id(value) {
+    if (this.idValue) {
+      throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    }
+    this.idValue = `#${value}`;
     this.validateOrder(2);
-    this.idCount += 1;
-    this.selectors.push(`#${value}`);
     return this;
   }
 
   class(value) {
+    this.classValues.push(`.${value}`);
     this.validateOrder(3);
-    this.selectors.push(`.${value}`);
     return this;
   }
 
   attr(value) {
+    this.attrValue = `[${value}]`;
     this.validateOrder(4);
-    this.selectors.push(`[${value}]`);
     return this;
   }
 
   pseudoClass(value) {
+    this.pseudoClassValues.push(`:${value}`);
     this.validateOrder(5);
-    this.selectors.push(`:${value}`);
     return this;
   }
 
   pseudoElement(value) {
+    if (this.pseudoElementValue) {
+      throw new Error('Element, id, and pseudo-element should not occur more than once inside the selector');
+    }
+    this.pseudoElementValue = `::${value}`;
     this.validateOrder(6);
-    this.pseudoElementCount += 1;
-    this.selectors.push(`::${value}`);
     return this;
   }
 
+  validateOrder(order) {
+    if (this.order <= order) {
+      this.order = order;
+    } else {
+      throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    }
+  }
+
   combine(selector1, combinator, selector2) {
-    this.selectors.push(`${selector1.stringify()} ${combinator} ${selector2.stringify()}`);
+    this.elementValue = `${selector1.stringify()} ${combinator} ${selector2.stringify()}`;
     return this;
   }
 
   stringify() {
-    const res = this.selectors.join('');
-    this.selectors = [];
-    return res;
-  }
-
-  validateOrder(prio) {
-    const lastSelector = this.selectors[this.selectors.length - 1];
-    let elementCount = 0;
-    let idCount = 0;
-    let pseudoElementCount = 0;
-
-    this.selectors.forEach((selector) => {
-      if (selector.startsWith('#')) {
-        idCount += 1;
-      } else if (selector.startsWith('::')) {
-        pseudoElementCount += 1;
-      } else {
-        elementCount += 1;
-      }
-    });
-
-    if (elementCount > 1 || idCount > 1 || pseudoElementCount > 1) {
-      throw new Error('Element, id and pseudo-element should not occur more than one time inside the selector');
-    }
-
-    if (
-      (lastSelector && lastSelector.startsWith('#') && prio !== 2)
-      || (lastSelector && lastSelector.startsWith('::') && prio !== 6)
-    ) {
-      throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
-    }
+    let selectorString = this.elementValue;
+    selectorString += this.idValue;
+    selectorString += this.classValues.join('');
+    selectorString += this.attrValue;
+    selectorString += this.pseudoClassValues.join('');
+    selectorString += this.pseudoElementValue;
+    return selectorString;
   }
 }
 
 const cssSelectorBuilder = {
   element(value) {
-    return new CssSelectorBuilder().element(value);
+    return new Selector().element(value);
   },
 
   id(value) {
-    return new CssSelectorBuilder().id(value);
+    return new Selector().id(value);
   },
 
   class(value) {
-    return new CssSelectorBuilder().class(value);
+    return new Selector().class(value);
   },
 
   attr(value) {
-    return new CssSelectorBuilder().attr(value);
+    return new Selector().attr(value);
   },
 
   pseudoClass(value) {
-    return new CssSelectorBuilder().pseudoClass(value);
+    return new Selector().pseudoClass(value);
   },
 
   pseudoElement(value) {
-    return new CssSelectorBuilder().pseudoElement(value);
+    return new Selector().pseudoElement(value);
   },
 
   combine(selector1, combinator, selector2) {
-    return new CssSelectorBuilder().combine(selector1, combinator, selector2);
+    const selector = new Selector();
+    return selector.combine(selector1, combinator, selector2);
   },
 };
-
 
 module.exports = {
   Rectangle,
